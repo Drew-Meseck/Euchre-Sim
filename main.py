d114c7ebab3eb277f836ce803778f81eabbe485d
Drew Meseck <dm3306a@student.american.edu>
2020-05-03 21:47:11 -0600

    Added Main.py and implemented card class

---
+++ main.py
@@ -0,0 +1,34 @@
+#Main File
+
+import numpy as np
+
+
+#Class definitions
+class Card:
+    def __init__(self, val, su):
+        self.value = val
+        self.suit = su
+
+    def show(self):
+        print(" {} of {} ".format(self.value, self.suit))
+
+class Deck:
+    pass
+
+class Player:
+    pass
+
+
+
+#Main function
+def main():
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
